f2cb606af6a713a77cc010ae034dee94e078526d
Jon Arrizabalaga <arrijon96@gmail.com>
2024-03-28 18:50:01 -0400

    removed cached folder

--- install/lib/cmake/matplotlibcpp_lib-config.cmake
+++
@@ -1,7 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-find_dependency(Python3 COMPONENTS Interpreter Development NumPy REQUIRED)
-find_dependency(PythonLibs 3.0 REQUIRED)
-
-get_filename_component(SELF_DIR ${CMAKE_CURRENT_LIST_DIR} PATH)
-include(${SELF_DIR}/cmake/matplotlibcpp_lib.cmake)
